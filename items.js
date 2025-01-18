4ad706a36211c538f5c9ed0d67ae227b6871e831
Srishanth-Kargaom <kargaomsrishanth@gmail.com>
2025-01-18 11:30:40 +0530

    Add files via upload

---
+++ items.js
@@ -0,0 +1,68 @@
+const items = {
+  electronics: [
+    {
+      type: "Mobile",
+      name: "iphone 14 pro max",
+      description: "most durable best and great for games and usage",
+      prise: "1,39,000",
+
+      imgsrc:
+        "https://iplanet.one/cdn/shop/files/r1598_Gold_PDP_Image_Position-1a_Avail__en-IN.jpg?v=1691141656&width=823",
+    },
+    {
+      type: "Laptop",
+      name: "MacBook Air M1",
+      description:
+        "Buy Samsung Galaxy M35 5G (Dark Blue) with 16GB Memory and 526GB Storage, 20000mAh battery, 13.1 inch main display.",
+      prise: "60,000",
+      imgsrc:
+        "https://www.apple.com/newsroom/images/product/mac/standard/Apple_new-macbookair-wallpaper-screen_11102020_big.jpg.large_2x.jpg",
+    },
+    {
+      type: "HeadPhones",
+      name: "Marshall HeadPhones",
+      description:
+        "True Wireless Active Noise Cancelling Bluetooth in Ear Earbuds with Transparency Mode and 30 Hours of Playtime - Black.",
+      prise: "14,999",
+      imgsrc:
+        "https://m.media-amazon.com/images/I/81D5DvVfSRL._SX679_.jpg",
+    },
+    {
+      type: "Speakers",
+      name: "Marshall speakers",
+      description:
+        "Marshall Woburn III Wired Connectivity Home Speaker with HDMI Input, Bluetooth 5.2 & RCA or 3.5mm Input - Black",
+      prise: "52,996",
+      imgsrc:
+        "https://m.media-amazon.com/images/I/91+udaNBWaL._SL1500_.jpg",
+    },
+    {
+      type: "Tablet",
+      name: "Aplle ipad pro(M4,2024)",
+      description:
+        " The new iPad Pro is impossibly thin, featuring outrageous performance with the Apple M4 chip, a breakthrough Ultra Retina XDR display, and superfast Wi-Fi 6E and 5G. ",
+      prise: "99,000",
+      imgsrc:
+        "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcQHPuWWf0JnWXejBhe_hiHQAoUrYRB5E7XN6T3wYXlWRYGCIsrYFXCZgiWFhXLePGQIw5fIkz3ENpIcHTx0bPDwj04BPpOQUdY5Oo8WVqs&usqp=CAE",
+    },
+    {
+      type: "AC",
+      name: "Lloyd AC",
+      description:
+        "Lloyd 1.5 Ton 3 Star Inverter Split AC (5 in 1 Convertible, Copper, Anti-Viral + PM 2.5 Filter, 2023 Model, White with Chrome Deco Strip, GLS18I3FWAGC)",
+      prise: "29890",
+      imgsrc:
+        "https://m.media-amazon.com/images/I/61Ka87z2DSL._SL1500_.jpg",
+    },
+    {
+      type: "ceiling fan",
+      name: "Geared HVLS Fan",
+      description:"AUSTAR AS series Fan are Geared Type HVLS Fans driven by German make Geared Motors, with five Aero Blades and Single piece die casted Hub",
+       
+      prise:"1,36,990",
+      imgsrc:
+        "https://shop.austarfansindia.com/wp-content/uploads/2022/07/geard-hvls-fan-1-300x300.webp",
+    },
+  ],
+};
+export default items;
